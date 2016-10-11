ddf2f250373d4a5af3a06720d9a0ca9ed5ac9bb3
Carlos Badenes <cbadenes@gmail.com>
2016-10-11 11:03:33 +0200

    listen for new Items

--- src/main/java/org/librairy/modeler/lda/eventbus/ItemBundledEventHandler.java
+++ src/main/java/org/librairy/modeler/lda/eventbus/ItemBundledEventHandler.java
@@ -9,11 +9,13 @@ package org.librairy.modeler.lda.eventbus;
 
 import org.librairy.model.Event;
 import org.librairy.model.domain.relations.Relation;
+import org.librairy.model.domain.resources.Resource;
 import org.librairy.model.modules.BindingKey;
 import org.librairy.model.modules.EventBus;
 import org.librairy.model.modules.EventBusSubscriber;
 import org.librairy.model.modules.RoutingKey;
 import org.librairy.modeler.lda.services.ModelingService;
+import org.librairy.storage.UDM;
 import org.librairy.storage.system.column.repository.UnifiedColumnRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,9 +29,9 @@ import javax.annotation.PostConstruct;
  * Created by cbadenes on 11/01/16.
  */
 @Component
-public class DomainContainsEventHandler implements EventBusSubscriber {
+public class ItemBundledEventHandler implements EventBusSubscriber {
 
-    private static final Logger LOG = LoggerFactory.getLogger(DomainContainsEventHandler.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ItemBundledEventHandler.class);
 
     @Autowired
     protected EventBus eventBus;
@@ -41,12 +43,12 @@ public class DomainContainsEventHandler implements EventBusSubscriber {
     protected Long delay;
 
     @Autowired
-    UnifiedColumnRepository columnRepository;
+    UDM udm;
 
     @PostConstruct
     public void init(){
-        BindingKey bindingKey = BindingKey.of(RoutingKey.of(Relation.Type.CONTAINS, Relation.State.CREATED),
-                "lda-modeler-domain-contain");
+        BindingKey bindingKey = BindingKey.of(RoutingKey.of(Relation.Type.BUNDLES, Relation.State.CREATED),
+                "lda-modeler-item-bundled");
         LOG.info("Trying to register as subscriber of '" + bindingKey + "' events ..");
         eventBus.subscribe(this,bindingKey );
         LOG.info("registered successfully");
@@ -54,13 +56,18 @@ public class DomainContainsEventHandler implements EventBusSubscriber {
 
     @Override
     public void handle(Event event) {
-        LOG.debug("Document added to domain event received: " + event);
+        LOG.debug("Item bundled event received: " + event);
         try{
             Relation relation = event.to(Relation.class);
 
             // BUNDLES relation
-            String domainUri = relation.getStartUri();
-            modelingService.train(domainUri,delay);
+            String documentUri  = relation.getStartUri();
+            String itemUri      = relation.getEndUri();
+
+            udm.find(Resource.Type.DOMAIN).from(Resource.Type.DOCUMENT,documentUri).forEach(domain -> {
+                modelingService.train(domain.getUri(),delay);
+            });
+
         } catch (Exception e){
             // TODO Notify to event-bus when source has not been added
             LOG.error("Error scheduling a new topic model for Items from domain: " + event, e);
