ecc31b4419dee95837bf17ebb717b64ff6001c99
Carlos Badenes <cbadenes@gmail.com>
2016-10-20 17:41:47 +0200

    handle minimum number of topics

--- src/main/java/org/librairy/modeler/lda/optimizers/BasicOptimizer.java
+++ src/main/java/org/librairy/modeler/lda/optimizers/BasicOptimizer.java
@@ -7,9 +7,7 @@
 
 package org.librairy.modeler.lda.optimizers;
 
-import org.librairy.computing.helper.SparkHelper;
 import org.librairy.modeler.lda.models.Corpus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
 
@@ -22,13 +20,11 @@ import org.springframework.stereotype.Component;
 @Conditional(BasicOptimizerCondition.class)
 public class BasicOptimizer implements LDAOptimizer{
 
-    @Autowired
-    SparkHelper sparkHelper;
-
     @Override
     public LDAParameters getParametersFor(Corpus corpus) {
         LDAParameters parameters = new LDAParameters();
-        parameters.setK(Double.valueOf(2*Math.sqrt(corpus.getSize()/2)).intValue());
+        Integer numTopics = Double.valueOf(2*Math.sqrt(corpus.getSize()/2)).intValue();
+        parameters.setK(numTopics != 0? numTopics : 2);
         parameters.setBeta(-1.0);
         parameters.setAlpha(-1.0);
         return parameters;
